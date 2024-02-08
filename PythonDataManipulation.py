fec9b5ec10ea294003b25805371f94330a3ad779
AksCode333 <47060340+AksCode333@users.noreply.github.com>
2024-02-08 10:39:34 -0600

    Update PythonDataManipulation.py
    matches the time list with the main list

--- PythonDataManipulation.py
+++ PythonDataManipulation.py
@@ -1,5 +1,7 @@
 test_list = [2, 3, 4, 1, 4, 5, 7, 8, 10, 2, 1, 4, 5, 3, 4, 1,2,3,4,5,6,7,8,9,10, 5, 11,2,  200, 2000, 4, 12020, 2012,2, 2,2,2,2,4]
-t = []
+time = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38]
+
+final_time = []
 final_lists = []
 def getIncreasingLists(list):
     results = []
@@ -15,11 +17,16 @@ def getIncreasingLists(list):
     t = results
     return results 
 
+def matchLists(t):
+    for i in t:
+        points = len(t[i])
+        print(points)
+
 def sepperateList(results):
-    print(results)
+    #print(results)
     for i in results:
-        print(i)
-        print(len(i))
+        #print(i)
+        #print(len(i))
         if len(i) >= 4:
             final_lists.append(i)
             print('')
@@ -29,6 +36,8 @@ def sepperateList(results):
 
 
 print(getIncreasingLists(test_list))
+print(matchLists(getIncreasingLists(test_list)))
 t = getIncreasingLists(test_list)
 print("new results")
 print(sepperateList(t))
+
